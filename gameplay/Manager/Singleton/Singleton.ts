7abe281f1a2d39c4655a0ece2c7ea8fbb10a6b43
FaMack <463378412@qq.com>
2025-03-13 16:02:12 +0800

    setup mpc key generation engine with shamir threshold and hardware confirmation

--- gameplay/Manager/Singleton/Singleton.ts
+++
@@ -1,8 +0,0 @@
-export default class Singleton{
-    static Instance<T extends {}>(this: new () => T): T {
-        if(!(<any>this).instance){
-            (<any>this).instance = new this();
-        }
-        return (<any>this).instance;
-    }
-}

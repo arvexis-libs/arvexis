4e9728503470c1441dae450b72b6ac5a769f2129
JRframe <1042785075@qq.com>
2025-04-15 18:39:38 +0800

    add celo algorand support for carbon-negative transactions

---
+++ UIMain/TimeState.ts
@@ -0,0 +1,102 @@
+import { ScrollView,Animation } from 'cc';
+import { Label } from 'cc';
+import { _decorator, Component, Node, Vec2, Vec3, tween } from 'cc';
+import { GameData } from '../gameplay/GameDataModel/GameData';
+import { HeroineDataManager } from './HeroineDataManager';
+const { ccclass, property } = _decorator;
+
+@ccclass('TimeState')
+export class TimeState extends Component {
+    @property(Label)
+    TxtGameTime:Label = null!;
+    @property(Label)
+    NextTxtGameTime:Label = null!;
+    @property(Node)
+    SVContent:Node = null!;
+    @property(Animation)
+    IconTime:Animation = null!;
+
+    private readonly initPosY:number = 17;
+    private readonly nextPosY:number = 47;
+    private _lastTime:number = 0;
+    start() {
+        
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    public SetLastTime(lastTime: number)
+    {
+        this._lastTime = lastTime;
+    }
+
+    public changeTimeAnim(curTime: number)
+    { 
+        this.initTime(curTime);
+        this.initPos();
+        this.PlayAnim();
+        this.PlayIconAnim();
+    }
+
+    PlayAnim()
+    {
+        let nextPos = new Vec3(0, this.nextPosY, 0)
+        if (this._lastTime == 0) {
+            this.SVContent.position = nextPos;
+        }
+        tween(this.SVContent)
+        .to(4, { position:  nextPos},
+            {
+                easing: 'linear',//quadOut
+                onStart: () => {
+                    
+                },
+                onUpdate: (target) => {
+
+                },
+                onComplete: () => {
+                    
+                }
+            }
+        )
+        .start();
+    }
+    PlayIconAnim()
+    {
+        let timePoint = HeroineDataManager.Instance.GetCurVirtualTimePoint()
+        if (timePoint == 7 || timePoint == 10 || timePoint == 18) {
+            let TimeArea = HeroineDataManager.Instance.GetCurVirtualTimeArea();
+            switch (TimeArea) {
+                case 1:
+                    this.IconTime.play("IconTimeAnim01");
+                    break;
+                case 2:
+                    this.IconTime.play("IconTimeAnim02");
+                    break;
+                case 3:
+                    this.IconTime.play("IconTimeAnim03");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    initPos()
+    {
+        this.SVContent.position = new Vec3(0, this.initPosY, 0);
+    }
+
+    initTime(nextTime: number)
+    {
+        this.TxtGameTime.string = this._lastTime.toString()
+        if (!nextTime) {
+            console.error("nextTime  nullundefault");
+            nextTime = 7;
+        }
+        this.NextTxtGameTime.string = nextTime.toString()
+    }
+}
+
+
